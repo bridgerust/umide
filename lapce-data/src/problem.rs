vaginessa/lapce

609: Collapse problem file

lapce-data/src/problem.rs
@@ -1,10 +1,14 @@
 use druid::WidgetId;
+use std::collections::HashMap;
+use std::path::PathBuf;
 
+#[derive(Clone)]
 pub struct ProblemData {
     pub widget_id: WidgetId,
     pub split_id: WidgetId,
     pub error_widget_id: WidgetId,
     pub warning_widget_id: WidgetId,
+    pub fold: HashMap<PathBuf, bool>,
 }
 
 impl ProblemData {
@@ -14,6 +18,7 @@ impl ProblemData {
             split_id: WidgetId::next(),
             error_widget_id: WidgetId::next(),
             warning_widget_id: WidgetId::next(),
+            fold: HashMap::new(),
         }
     }
 }

bridgerust/umide

lapce-data/src/problem.rs
@@ -1,10 +1,14 @@
 use druid::WidgetId;
+use std::collections::HashMap;
+use std::path::PathBuf;
 
+#[derive(Clone)]
 pub struct ProblemData {
     pub widget_id: WidgetId,
     pub split_id: WidgetId,
     pub error_widget_id: WidgetId,
     pub warning_widget_id: WidgetId,
+    pub collapsed: HashMap<PathBuf, bool>,
 }
 
 impl ProblemData {
@@ -14,6 +18,7 @@ impl ProblemData {
             split_id: WidgetId::next(),
             error_widget_id: WidgetId::next(),
             warning_widget_id: WidgetId::next(),
+            collapsed: HashMap::new(),
         }
     }
 }
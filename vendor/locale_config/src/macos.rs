49371465bec7f4d19e9c0822f1132c03a75f185e
dev-josias <kologojosias@gmail.com>
2026-03-03 05:56:09 +0000

    build: include remaining vendored path dependencies

---
+++ vendor/locale_config/src/macos.rs
@@ -0,0 +1,12 @@
+//! Inspect macOS system for locale configuration
+use super::{LanguageRange, Locale};
+
+use objc2_foundation::NSLocale;
+
+pub fn system_locale() -> Option<Locale> {
+    let current_locale = unsafe { NSLocale::currentLocale() };
+    let locale_identifier = unsafe { current_locale.localeIdentifier() };
+    Some(Locale::from(
+        LanguageRange::from_unix(&locale_identifier.to_string()).ok()?,
+    ))
+}
